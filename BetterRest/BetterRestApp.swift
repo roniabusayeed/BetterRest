29efaebcad0715b5c026e966e94dd6091e5bcf43
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-08-05 14:28:38 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Abu Sayeed Roni on 2023-08-05.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
